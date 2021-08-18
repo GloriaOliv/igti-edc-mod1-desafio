cc5647e65da11936bcab6e7b407c3a770ab7130b
GloriaOliv <mariagloria.batista@yahoo.com>
2021-08-18 19:24:05 -0300

    Atualizacao dos arquivos job_spark.py e provider.tf

--- etl/job_spark.py
+++ etl/job_spark.py
@@ -28,5 +28,5 @@ censo = (
     .mode("overwrite")
     .format("parquet")
     .partitionBy("NU_ANO_CENSO")
-    .save("s3://datalake-gloria-igti-edc-desafio/staging-zone/")
+    .save("datalake-gloria-igti-edc-desafio-producao-556116348126/staging-zone/")
 )
